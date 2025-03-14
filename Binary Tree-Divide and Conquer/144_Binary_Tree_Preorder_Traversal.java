681b4bd671c11c3e900a171626aaa5ec8c46995b
Zhujun Wang <zhujunwang@salesforce.com>
2025-03-14 16:13:44 -0700

    preorder check

--- Binary Tree-Divide and Conquer/144_Binary_Tree_Preorder_Traversal.java
+++ Binary Tree-Divide and Conquer/144_Binary_Tree_Preorder_Traversal.java
@@ -27,7 +27,28 @@ public class Solution {
             }
         }
         return res;
-        
-       
+    }
+}
+
+public class Solution {
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+       Stack<TreeNode> stack = new Stack<>();
+       List<Integer> res = new ArrayList<Integer>();
+        if(root == null) {
+            return res;
+        }
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return res;
     }
 }
